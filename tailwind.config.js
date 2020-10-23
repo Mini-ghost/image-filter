36e463eacf285d0cae36b3f204778a0fcd7bb077
Alex Liu <dsa1314@gmail.com>
2020-10-23 18:13:43 +0800

    feat: 導入 tailwindcss
    - 導入 tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ['./static/**/*.html']
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
